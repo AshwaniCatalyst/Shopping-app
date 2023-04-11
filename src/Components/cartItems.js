b9c7b79ac99303527f5a267b347ad442761566d9
AshwaniCatalyst <ashwani.sood@catalyst.com>
2023-04-11 11:49:03 +0530

    added functionality to display Empty Cart text when cart is empty

--- src/Components/cartItems.js
+++ src/Components/cartItems.js
@@ -1,13 +1,13 @@
-import { Button, Typography } from '@mui/material'
-import React from 'react'
-import { useDispatch, useSelector } from 'react-redux'
-import { clearCart } from '../Services/Actions/action'
+import { Button, Typography } from "@mui/material";
+import React from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { clearCart } from "../Services/Actions/action";
 
 const CartItems = () => {
-  const CartItems = useSelector((state)=>state.cartItems.productData)
+  const CartItems = useSelector((state) => state.cartItems.productData);
   const dispatch = useDispatch();
   // function quantityCounter(arr, id)
-  // { 
+  // {
   //   const existId = arr.find(item=> item.id===id)
   //   if(existId)
   //   {
@@ -16,25 +16,43 @@ const CartItems = () => {
   // }
   return (
     <>
-    {
-      CartItems.map((productDetails,index)=>{
-        // const itemCounter = quantityCounter(productDetails,productDetails.id);
-        return(
-          <div key={index} className='flex cartProductOuterContainer'>
-          <div >
-              <img src={productDetails.images[0]} alt='ProductImage' className='product-Image'/>
+      <div className="cartOuterContainer">
+        {
+          // console.log(CartItems.length)
+          CartItems.length === 0 && (
+            <div>
+              <h4> No Items Added</h4>
+            </div>
+          )
+        }
+        {CartItems.map((productDetails, index) => {
+          // const itemCounter = quantityCounter(productDetails,productDetails.id);
+          return (
+            <div key={index} className="flex cartProductOuterContainer">
+              <div>
+                <img
+                  src={productDetails.images[0]}
+                  alt="ProductImage"
+                  className="product-Image"
+                />
+              </div>
+              <Typography variant="h4">{productDetails.title}</Typography>
+              <Typography variant="h4">${productDetails.price}</Typography>
+              <Typography variant="h4">x1</Typography>
+            </div>
+          );
+        })}
+        {CartItems.length >= 1 && (
+          <div className="cartButtons">
+            <Button variant="contained" onClick={() => dispatch(clearCart())}>
+              Clear Cart
+            </Button>
+            <Button variant="contained">Place Order</Button>
           </div>
-          <Typography variant='h4'>{productDetails.title}</Typography>
-          <Typography variant='h4'>${productDetails.price}</Typography>
-          <Typography variant='h4'>x1</Typography>
-        </div>
-        )
-      })
-    }
-    <Button variant='contained' onClick={()=>dispatch(clearCart())}>Clear Cart</Button>
-    <Button variant='contained'>Place Order</Button>
+        )}
+      </div>
     </>
-  )
-}
+  );
+};
 
-export default CartItems
+export default CartItems;
